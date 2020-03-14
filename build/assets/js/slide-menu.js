1189f47c266db87b24de65fad57ce3f589a45970
ndicecco <32521585+ndicecco@users.noreply.github.com>
2020-03-14 16:34:17 -0700

    finished sidebar nav

--- build/assets/js/slide-menu.js
+++ build/assets/js/slide-menu.js
@@ -1,1 +1,13 @@
-function openSlideMenu(){console.log("Hello"),document.getElementById("menu").style.width="250px",document.getElementById("content").style.marginLeft="250px"}function closeSlideMenu(){console.log("Hello"),document.getElementById("menu").style.width="0",document.getElementById("content").style.marginLeft="0"}
+function openSlideMenu() {
+    document.getElementById('menu').style.width = '250px';
+    document.getElementById('menu').style.opacity = '1';
+    document.getElementById('content').style.marginLeft = '250px';
+    document.getElementById('hamburger').style.opacity = '0.1';
+    
+  }
+function closeSlideMenu() {
+    document.getElementById('menu').style.width = '0';
+    document.getElementById('menu').style.opacity = '0';
+    document.getElementById('content').style.marginLeft = '0';
+    document.getElementById('hamburger').style.opacity = '0.9';
+  }
--- source/js/slide-menu.js
+++ source/js/slide-menu.js
@@ -1,10 +1,11 @@
+const hamburger = document.querySelector('.slide');
+
 function openSlideMenu() {
-    console.log("Hello");
     document.getElementById('menu').style.width = '250px';
     document.getElementById('content').style.marginLeft = '250px';
+    hamburger.classList.toggle("open");
   }
 function closeSlideMenu() {
-    console.log("Hello");
     document.getElementById('menu').style.width = '0';
     document.getElementById('content').style.marginLeft = '0';
   }
