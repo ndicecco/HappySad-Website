dec9a553e3e641d1d1973fddfdf57fc144b696fc
ndicecco <32521585+ndicecco@users.noreply.github.com>
2020-04-03 17:01:49 -0700

    too many updates to list

--- source/js/app.js
+++ source/js/app.js
@@ -1,10 +1,28 @@
-import Swup from 'swup';
-const swup = new Swup();
+
+const carouselSlide = document.querySelector('.slide');
+const carouselImages = document.querySelectorAll('.slide img');
+
+//Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+
+//Button Listeners
+function nextImage(){
+    counter++;
+    console.log(counter);
+};
+
 
 //GSAP Animations
-var tl = gsap.timeline();
-tl.from(".headline", {duration: 2, opacity: 0}, "0.8");
-tl.from(".grid-item-1", {duration: 1, x: -5, y: -5, opacity: 0}, 0);
-tl.from(".grid-item-2", {duration: 1, x: 5, y: -5, opacity: 0}, 0.2);
-tl.from(".grid-item-3", {duration: 1, x: -5, y: 5, opacity: 0}, 0.4);
-tl.from(".grid-item-4", {duration: 1, x: 5, y: 5, opacity: 0}, 0.6);
+// var tl = gsap.timeline();
+// tl.from(".headline", {duration: 2, opacity: 0}, "0.8");
+// tl.from(".grid-item-1", {duration: 1, x: -5, y: -5, opacity: 0}, 0);
+// tl.from(".grid-item-2", {duration: 1, x: 5, y: -5, opacity: 0}, 0.2);
+// tl.from(".grid-item-3", {duration: 1, x: -5, y: 5, opacity: 0}, 0.4);
+// tl.from(".grid-item-4", {duration: 1, x: 5, y: 5, opacity: 0}, 0.6);
---
+++ source/js/musicbox.js
@@ -0,0 +1,91 @@
+//Music player for sound design page
+var waltz = "./assets/audio/waltz.wav";
+var ghost = "./assets/audio/ghost.wav";
+var books = "./assets/audio/books.wav";
+var train = "./assets/audio/train.wav";
+var tube = "./assets/audio/tube.mp3";
+var wobble = "./assets/audio/wobble.wav";
+var jenga = "./assets/audio/jenga.wav";
+var whale = "./assets/audio/whale.wav";
+var uptown = "./assets/audio/uptown.wav";
+var jingle = "./assets/audio/jingle.mp3";
+var halls = "./assets/audio/halls.mp3";
+var away = "./assets/audio/away.mp3";
+var tunes = [waltz, ghost, books, train, tube, wobble, jenga, whale, uptown, jingle, halls, away];
+var fillBar = '';
+var lastClicked = 'word';
+var lastReset = 'word';
+var sound = new Audio();
+
+function playOrPauseSong(d) {
+    //d is the id of the controller that was just clicked
+    var d = d;
+    if (isNaN(lastClicked)) {
+        //checks to see if this is the first sound you've clicked since loading the page
+        //if it is, great! Move on to loading the sound
+    } else if(sound.currentTime !== 0 && d == lastClicked && lastReset !== d) {
+        //checks if a sound is mid playback AND the sound controller you just clicked is the same as the last one you clicked AND the last reset used is also part of this controller
+        //if so, great! Move on to loading the sound
+    } else if(sound.currentTime == 0 && d == lastClicked && lastReset == d) {
+        //checks if the loaded sound has been reset to 0 AND what you just clicked is the same controller as lastClicked AND what you last reset is the same controller
+        //if so, great! Move on to loading the sound
+    } else if(sound.currentTime !== 0 && d == lastClicked && lastReset == d && sound.paused == false){
+        //checks if the loaded sound is mid playback AND the justClicked is the same controller as lastClicked AND as lastReset AND the song is not currently paused
+        //in this case, you have reset this controller but you played the sound again, so you're probably trying to pause the song
+        sound.pause();
+        document.querySelector('#s' + d + ' img').setAttribute('src', './assets/img/play.png');
+        return;
+    } else if(sound.currentTime !== 0 && d == lastClicked && d == lastReset && sound.paused == true){
+        //checks if the loaded sound is mid playback AND the last button you clicked is the same as the last one you reset AND the sound is paused
+        //you're probably trying to play the sound you paused after resetting it at least once
+        sound.play();
+        document.querySelector('#s' + d + ' img').setAttribute('src', './assets/img/pause.png');
+        return;
+    } else {
+        //if none of the above is true, then you must have interacted with the play button of a new controller, so we should reset the sound of whatever controller was last used
+        resetSong(lastClicked);
+    };
+    if (lastClicked !== d) {
+        sound = new Audio();
+    };
+    lastClicked = d;
+    var i = tunes[d];
+    if (sound.readyState == 0) {
+        sound = new Audio(i);
+        sound.play();
+        document.querySelector('#s' + d + ' img').setAttribute('src', './assets/img/pause.png');
+        sound.addEventListener('timeupdate', function () {
+            fillBar = document.getElementById("fill" + d);
+            var position = sound.currentTime / sound.duration;
+            fillBar.style.width = position * 100 + '%';
+        });
+    } else if (sound.readyState == 4 && sound.paused) {
+        sound.play();
+        document.querySelector('#s' + d + ' img').setAttribute('src', './assets/img/pause.png');
+    } else {
+        sound.pause();
+        document.querySelector('#s' + d + ' img').setAttribute('src', './assets/img/play.png');
+    }
+}
+
+function resetSong(d) {
+    //d is the id of the controller that was just clicked
+    lastReset = d;
+    if(lastClicked !== lastReset){
+        return;
+    }
+    if(lastClicked == lastReset){
+    }
+    clearFills();
+    sound.pause();
+    sound.currentTime = 0;
+    document.querySelector('#s' + lastClicked + ' img').setAttribute('src', './assets/img/play.png');
+    document.querySelector('#s' + d + ' img').setAttribute('src', './assets/img/play.png');
+}
+
+function clearFills() {
+    for (let i = 0; i < tunes.length; i++) {
+        var fillBar = document.getElementById("fill" + i);
+        fillBar.style.width = 0;   
+    };
+}
