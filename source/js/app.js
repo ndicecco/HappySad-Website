56398eae415d2bdf5a6e78961fd6ad25d4fa1fd0
ndicecco <32521585+ndicecco@users.noreply.github.com>
2020-04-04 13:07:04 -0700

    got paperwork carousel half working

--- source/js/app.js
+++ source/js/app.js
@@ -1,25 +1,49 @@
 import Swup from 'swup';
 const swup = new Swup();
 
-// const carouselSlide = document.querySelector('.slide');
-// const carouselImages = document.querySelectorAll('.slide img');
+//Carousel for Stage Management page
+const carouselSlide = document.querySelector('.paperslide');
+const carouselImages = document.querySelectorAll('.paperslide img');
 
-// //Buttons
-// const prevBtn = document.querySelector('#prevBtn');
-// const nextBtn = document.querySelector('#nextBtn');
+//Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
 
-// //Counter
-// let counter = 1;
-// const size = carouselImages[0].clientWidth;
+//Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
 
-// carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+//Button Listeners
+nextBtn.addEventListener('click', ()=>{
+    if(counter >= carouselImages.length - 1) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+});
 
-// //Button Listeners
-// function nextImage(){
-//     counter++;
-//     console.log(counter);
-// };
+prevBtn.addEventListener('click', ()=>{
+    if(counter <=0) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+});
 
+carouselSlide.addEventListener('transitionend', ()=>{
+    if(carouselImages[counter].id==='lastClone'){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length-2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+    }
+    if(carouselImages[counter].id==='firstClone'){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length-counter;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
+    }
+});
+
+
+
+//--- end carousel script
 
 //GSAP Animations
 // var tl = gsap.timeline();
