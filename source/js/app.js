5e93fd687bfb000fef85b91c62776c221e028458
ndicecco <32521585+ndicecco@users.noreply.github.com>
2020-04-27 18:43:19 -0700

    got navigation and animation working on portfolio

--- source/js/app.js
+++ source/js/app.js
@@ -2,14 +2,136 @@ import Swup from 'swup';
 const swup = new Swup();
 
 function init() {
+  if (document.querySelector('#portfolio-selector')) {
+    const navArray = document.querySelectorAll('.portfolio-menu a');
+    const btnContainer = document.querySelector('.portfolio-menu');
+    const btns = btnContainer.getElementsByClassName('port-btn');
+    const showArray = document.querySelectorAll('.show');
+    const smArray = document.querySelectorAll('.sm');
+    const pmArray = document.querySelectorAll('.pm');
+    const sdArray = document.querySelectorAll('.sd');
+    const pwGallery = document.querySelector('#paperwork-gallery');
+    const pastPro = document.querySelector('#past-productions-title');
+
+    let justClicked = '';
+
+    //hide the paperwork gallery
+    pwGallery.style.display = 'none';
+
+    function addListeners() {
+      for (let j = 0; j < navArray.length; j++) {
+        navArray[j].addEventListener("click", showMe);
+      }
+    }
+
+    function showMe() {
+      //grab the id and class of whatever was just clicked
+      justClicked = this.id;
+
+
+      // Loop through the nav and add the active class to the current/clicked button
+      for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+          var current = document.getElementsByClassName("active");
+          current[0].className = current[0].className.replace(" active", "");
+          this.className += " active";
+        });
+      }
+
+      gsap.to('#paperwork-gallery', {
+        duration: 1,
+        opacity: 0
+      });
+      //animate show grid items to 0 opacity
+      gsap.to('.show', {
+        duration: 1,
+        opacity: 0,
+        stagger: {
+          grid: [7, 3],
+          from: 11,
+          ease: "power3.in",
+          amount: 0.5
+        },
+        onComplete: switchIt,
+        onCompleteParams: [justClicked]
+      });
+
+      function switchIt(justClicked) {
+        //reset all of the show grid items to display: none
+        for (var i = 0; i < showArray.length; i++) {
+          showArray[i].style.display = 'none';
+        }
+        //make sure that past productions title is visible
+        pastPro.style.opacity = '1';
+        //hide the paperwork gallery
+        pwGallery.style.display = 'none';
+        //display based on which link was click
+        switch (justClicked) {
+          case 'all':
+            for (var i = 0; i < showArray.length; i++) {
+              showArray[i].style.display = 'initial';
+            }
+            gsap.to('.show', {
+              duration: 1,
+              opacity: 1
+            });
+            break;
+          case 'sm':
+            for (var i = 0; i < smArray.length; i++) {
+              smArray[i].style.display = 'initial';
+            }
+            gsap.to('.sm', {
+              duration: 1,
+              opacity: 1
+            });
+            break;
+          case 'pm':
+            for (var i = 0; i < pmArray.length; i++) {
+              pmArray[i].style.display = 'initial';
+            }
+            gsap.to('.pm', {
+              duration: 1,
+              opacity: 1
+            });
+            break;
+          case 'sd':
+            for (var i = 0; i < sdArray.length; i++) {
+              sdArray[i].style.display = 'initial';
+            }
+            gsap.to('.sd', {
+              duration: 1,
+              opacity: 1
+            });
+            break;
+          case 'pw':
+            pastPro.style.opacity = '0';
+            pwGallery.style.display = 'initial';
+            gsap.to('#paperwork-gallery', {
+              duration: 1,
+              opacity: 1
+            });
+            break;
+
+          default:
+            break;
+        }
+      }
+
+
+    }
+
+    addListeners();
+    //end of portfolio selector
+  }
   if (document.querySelector('.papercon')) {
     //initialize baguetteBox
     baguetteBox.run('.papercon');
-
     //magnific pop-up
-    $('.open-popup-link').magnificPopup({
-      type:'inline',
-      midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    $(document).ready(function () {
+      $('.open-popup-link').magnificPopup({
+        type: 'inline',
+        midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+      });
     });
   }
   if (document.querySelector('#content')) {
@@ -86,6 +208,7 @@ function init() {
     addListeners();
     //end HappyNav
   }
+
 }
 
 //initialize scripts after document loads
